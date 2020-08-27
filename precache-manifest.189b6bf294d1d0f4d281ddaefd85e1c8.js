10568c591e992b9becb23ddba66978fbf7ef6eed
khaledsaidfcis <khaledsaidfcis@gmail.com>
2020-08-27 21:50:28 +0200

    Updates

---
+++ precache-manifest.189b6bf294d1d0f4d281ddaefd85e1c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e540f16c6e7be79e8dbebc08085c3c0",
+    "url": "/React_P/index.html"
+  },
+  {
+    "revision": "967923a3b09deafd4dda",
+    "url": "/React_P/static/css/main.110839eb.chunk.css"
+  },
+  {
+    "revision": "0e72a38fa4d9c7b9175a",
+    "url": "/React_P/static/js/2.81f9b36c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_P/static/js/2.81f9b36c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967923a3b09deafd4dda",
+    "url": "/React_P/static/js/main.b0c60d48.chunk.js"
+  },
+  {
+    "revision": "1cb2affe67c74e540d8e",
+    "url": "/React_P/static/js/runtime-main.e46bf0cd.js"
+  }
+]);
